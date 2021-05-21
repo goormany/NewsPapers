a8b926636cefc72d2ca3023c403195be542844f5
goormany <zarbot351@yandex.ru>
2021-05-21 19:31:30 +0300

    add bootstrap 2.0

---
+++ news/models.py
@@ -0,0 +1,95 @@
+from django.db import  models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    authorRaitng = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = self.post_set.aggregate(postRating=Sum('rating'))
+        postRat = 0
+        postRat += post_rating.get('postRating')
+
+        comment_rating = self.authorUser.comment_set.aggregate(commentRating=Sum('rating'))
+        comRat = 0
+        comRat += comment_rating.get('commentRating')
+
+        self.authorRating = postRat * 3 + comRat
+        self.save()
+
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Post(models.Model):
+    PostAuthor = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    PostNews = 'PN'
+    PostArticle = 'PA'
+
+    POSITIONS = [
+        (PostArticle, 'Статья'),
+        (PostNews, 'Новость'),
+    ]
+    title = models.CharField(max_length=50)
+    positions = models.CharField(max_length=2, choices=POSITIONS, default=PostArticle)
+    data = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    previewName = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating +=1
+        self.save()
+
+    def dislike(self):
+        self.rating -=1
+        self.save()
+
+    def preview(self):
+        return self.text[0:124] + '...'
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+class PostCategory(models.Model):
+    pcPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    pcCategory = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Пост категория'
+        verbose_name_plural = 'Пост категории'
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    commentText = models.TextField(max_length=124)
+    dataCreating = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating +=1
+        self.save()
+
+    def dislike(self):
+        self.rating -=1
+        self.save()
+
+    class Meta:
+        verbose_name = 'Коммент'
+        verbose_name_plural = 'Комменты'
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post, PostCategory, Author, Category, Comment
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class news(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
