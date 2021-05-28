9f0281db0b46b3d07feb0595025ac745abf17a4f
goormany <zarbot351@yandex.ru>
2021-05-28 03:25:04 +0300

    rename: news/del -> news/delete

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:pk>', new.as_view(), name='news_detail'),
     path('search', newSearch.as_view(), name='news_search'),
     path('add/', newsCreate.as_view(), name='news_add'),
-    path('<int:pk>/del/', newsDelete.as_view(), name='news_delete'),
+    path('<int:pk>/delete/', newsDelete.as_view(), name='news_delete'),
     path('<int:pk>/edit/', newsUpdate.as_view(), name='news_update'),
 ]
