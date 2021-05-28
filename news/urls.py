8f950a8332e423386bef0c04a8dfcc6b5c59b0e4
goormany <zarbot351@yandex.ru>
2021-05-28 03:14:25 +0300

    fix news/del, news/edit

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:pk>', new.as_view(), name='news_detail'),
     path('search', newSearch.as_view(), name='news_search'),
     path('add/', newsCreate.as_view(), name='news_add'),
-    path('del/<int:pk>', newsDelete.as_view(), name='news_delete'),
-    path('edit/<int:pk>', newsUpdate.as_view(), name='news_update'),
+    path('<int:pk>/del/', newsDelete.as_view(), name='news_delete'),
+    path('<int:pk>/edit/', newsUpdate.as_view(), name='news_update'),
 ]
