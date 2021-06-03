bb8981a2fbd5bb73917dead2dbff668782e6ace0
goormany <zarbot351@yandex.ru>
2021-06-03 22:57:13 +0300

    register v1.0

--- news/views.py
+++ news/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.core.paginator import Paginator
 from .filters import newsFilter
 from .forms import NewsForm
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
 class news(ListView):
     model = Post
@@ -13,10 +13,11 @@ class news(ListView):
     paginate_by = 5
 
 
-class new(DetailView):
+class new(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Post
     template_name = 'new_home.html'
     context_object_name = 'new'
+    permission_required = 'news.view_post'
 
 
     def get_context_data(self, **kwargs):
@@ -24,30 +25,31 @@ class new(DetailView):
         context['com_post'] = Comment.objects.filter(commentPost=self.kwargs['pk']).values("commentText")
         return context
 
-class newSearch(ListView):
+class newSearch(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Post
     template_name = 'new_search.html'
     context_object_name = 'newsearch'
     ordering = '-data'
+    permission_required = 'news.view_post'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = newsFilter(self.request.GET, queryset=self.get_queryset())
         return context
 
-class newsCreate(CreateView, PermissionRequiredMixin):
+class newsCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'news/news_add.html'
     form_class = NewsForm
     permission_required = 'news.add_post'
 
-class newsDelete(DeleteView, PermissionRequiredMixin):
+class newsDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     template_name = 'news/news_delete.html'
     queryset = Post.objects.all()
     context_object_name = 'newd'
     success_url = '/news/'
     permission_required = 'news.delete_post'
 
-class newsUpdate(UpdateView, PermissionRequiredMixin):
+class newsUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'news/news_update.html'
     form_class = NewsForm
     permission_required = 'news.change_post'
