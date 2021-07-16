b9ea71ef790952fa42b2e6c71522cce4eaba4355
goormany <zarbot351@yandex.ru>
2021-07-16 23:32:01 +0300

    edit template

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -88,11 +88,7 @@ COMMENT_FLAG_REASONS = [
     (4, ('Своя причина')),
 ]
 
-LOCALE_PATH = [
-    os.path.join(BASE_DIR, 'locale')
-]
 
-USE_I18N = True
 USE_L18N = True
 COMMENT_ALLOW_TRANSLATION = True
 
@@ -207,6 +203,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATH = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
